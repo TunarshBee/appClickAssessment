743565abd5359881bbe30278d78e95d89197758f
TunarshBee <rafiubello4real@gmail.com>
2022-09-22 10:22:41 +0100

    direct authentication

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import {
     Routes,
     Route,
     Navigate,
-    // Link,
+    
 } from "react-router-dom";
 
 import Index from "./components/UserId/Index";
-import Home from "./components/Home/Home";
 import FormTemplates from "./components/AppInputs/FormTemplates";
 import Preview from "./components/Preview/Preview";
+import Form from "./components/Form";
 function App() {
     const theuser = useSelector((state) => state.RegisterUser.username);
     return (
@@ -20,13 +20,13 @@ function App() {
                 
                 <Router>
                     <Routes>
-                        <Route path="/home" element={<Home />} />
+                        <Route path="/templates" element={<Form/>} />
                         <Route path="/preview" element={<Preview />} />
                         <Route
                             path="/"
                             element={
                                 theuser ? (
-                                    <Navigate replace to="/home" />
+                                    <Navigate replace to="/templates" />
                                 ) : (
                                     <Index />
                                 )
